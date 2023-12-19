2fd3398fcc1c28d7849b46dd35a0d1aeb8284e1c
STR <92766928+LaloQuera710@users.noreply.github.com>
2023-12-18 18:34:26 -0600

    Add files via upload

---
+++ Qro.sh
@@ -0,0 +1,34 @@
+ddddkOkx;'lxc'........,lodooool:'.',::;  
+dddxOOkxc;oxl'........;odoooooool:'',;;;:
+dddxkkkxoldxl'........;odooddooodol;',,',;::
+xddxxkkkxkkkl'........;oddddddddoodoc,''',,,,:               @@@@@@   @@@@@@@   @@@@@@     @@@@@@@@@@  @@@  @@@ 
+xxxxkkkkkOOkc.........,ldddddddddddddc,''''..',;            @@!  @@@  @@!  @@@ @@!  @@@    @@! @@! @@! @@!  !@@ 
+xxkkOOOOOkOk:.........'lddddddddddddddc,','....',:         @!@  !@!  @!@!!@!  @!@  !@!    @!! !!@ @!@  !@@!@!  
+xxkO000OOkOk:.........,oxddddddddddddddl:;'.....',;;;      !!:!!:!:  !!: :!!  !!:  !!!    !!:     !!:  !: :!!  
+xxkOK00OOOkd;.........;oxddddddddddddddxdl;..'...',,,;::    : :. :::  :   : :  : :. :      :      :   :::  ::: 
+kxxOK00000Od,.........;oxxxxxdxxddxxddxxkxc'.,'..,cll:,,;;
+kxx0K00O00Oo,.'.......;dxxxxxxxxxxxxxxxkkkd;:d:..':oxdl;',,',
+kxxO0OkOOOko'.'''''...;dkxxxxxxxxxxxxxxkkkxlok:..,;oxxxo,','.';
+kxxk0OOOOOxl'.'''.'''.:xkxxxxxxxxxxxxxkkkkkxxOo'.',lkxxxo;''..',;
+kxxO0K0O0Oxc...''..''.:xkkkkkkkkkkkkkkkkxxxkOOo'...:xkkkkdc'..,c;'                                 @@@@@@ @@@@@@@ @@@@@@@ 
+kkO0000OOkxc.'....''..ckkkkkkkkkkkkkkkkkkkkkOOo'...:xOkkOkd:,,'ld:''':                            !@@       @!!   @@!  @@@ 
+OkO00OOOOOkc....'..'..ckOkkkkkkkkkkkkkkkkkkkO0o,...:xOkkO0kodo':kd:'.',:                           !@@!!    @!!   @!@!!@!  
+OkO000OOOOx:...''..'..lOOOOOOOOOOOOOOOkkxkkkOOd,...;xOOO00Okkx,;xOx:.',,',                            !:!   !!:   !!: :!!  
+OO0000OOOOx:...''''..'oOOOOOOOOOOOOOOOOkOOOkOOd,...;x0OOO0OOOk;'dOOdc;:l;.',:                     ::.: :     :     :   : : 
+OO000OO00Ox;.''..'...,oOOOOOOOOO0000KK0kO0OO0Od,...;k0OOOO000O:'oOOOkl:xo;;;'';
+00000O000Oo,.'...'...'d0O00O0000000000OkkOOkkOd,...;k000O0K0KOc'lOOOOo:dOxoll;'';
+0000000000d,..''''''.'dOOKKK0KKK0KK00K0OOOkkkOx;...;k000O0000Oc.cOOkkd:d0OoxOlcl;,,
+OO0000000Oo,.','''''''coodxxoooolllcoxkkOkkkkOx;...,d0KK0kOOOOl'cOkkkd:o0Odx0ooxdoc::;
+xO0OO00000d'.''''''''..',;::;:lllc;;;,:dkkxxxkx;....;cloxkOOOOo',dkkkx:o0OoxKolddxcclollldkd:;,
+dk000000OOo''''''''.'..'',;:,,cool:coooxkxxkkkx;...'loccdO0000d',dOOOk:l0Olo0dcddxxkodo:cool;;:;
+xkOO0000kkl'...'''....,:'.':,.',,;;cOXKOkxxxxkx:...'oxllxOOO00d,.lkkOkcl00olOo:odoocccoxxl:::,
+xkkkOOOOkkl...''......,;,,;:;,:lloookOOkkkkkOOkc'...,:::cloodxo,'cddxkolxxl:l:,:c::;;,'',;
+kkkO000Okko,''',''''',:c:;,''''',,;,;;;::cccccc:,'',,''',,,,;;;;;;:::llcccccc;',;;;;:
+lllllllcc:;;,,,,',,,,,,,,,,,,,,,,,,;;,;;;;;;;;;::::::::::::::::::;;::::::::::;;:
+'''''''''',,,,,,,,,,,;;;;;;::::::;;;;;;;;;;;;;;,,,,,,,,,,,'''',,,,,,,,,,,,;
+''''''''''''.''''.''''',,,,,;;;;;;:::::ccccllllloooooolc:;;;;;;;;,,,,,,;:l
+;;:::::::ccccccclllllooooooddddddddxxxxxxxxxddoollccc:;;,,,,,,,'''
+ooooooooooodddddddddddddddddxxxxddddoooollcc::;;,,,,,,,,,,,'',;
+ddddddddddddddxxxxxxxddoolllccccccc::;;;;;,,,,,,,,,,''''',;:
+dddddddddddoollcc::;;;;;,;;;;;;;;;,,,,,,,,'','''''''',,:
+ddddollc:;,,'....'',;;:::::;,,,,,,,,''',''''''''''',;
